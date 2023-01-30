40cf30502b0067246d6440671316c50eda26680c
Paul Bureau <paul.bureau@etu.u-bordeaux.fr>
2023-01-30 14:56:35 +0100

    Création des fichiers aes.c et aes.h + création des S-box

---
+++ aes.h
@@ -0,0 +1,13 @@
+/**
+ * @file aes.h
+ * @brief AES (Rijndael) block cipher implementation
+ * @authors Paul Bureau, Victoria Martini, Quentin Bonnel
+**/
+
+#ifndef AES_H
+#define AES_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+#endif /* AES_H */
