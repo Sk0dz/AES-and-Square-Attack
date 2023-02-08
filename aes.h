8d864a5dda41af41a9bba857b001cd41018ba963
Paul Bureau <paul.bureau@etu.u-bordeaux.fr>
2023-02-08 13:51:10 +0100

    Ajout clang format

--- aes.h
+++ aes.h
@@ -2,7 +2,7 @@
  * @file aes.h
  * @brief AES (Rijndael) block cipher implementation
  * @authors Paul Bureau, Victoria Martini, Quentin Bonnel
-**/
+ **/
 
 #ifndef AES_H
 #define AES_H
@@ -15,7 +15,7 @@
  * @details Replace each byte of the state by his equivalent using the gived S-Box
  * @param state the current state of the cipher
  * @pre @p state must be a valid pointer toward an array with a size of Nb * 4
-**/
+ **/
 void sub_bytes(uint8_t *state);
 
 #endif /* AES_H */
