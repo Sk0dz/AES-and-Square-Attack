3aa06e9c8293f33da1c102ca469a60c6e476a67b
Paul Bureau <paul.bureau@etu.u-bordeaux.fr>
2023-02-16 11:34:54 +0100

    Ajout de la multiplication dans GF(2^8)

--- gmath.c
+++ gmath.c
@@ -1,3 +1,25 @@
 #include "gmath.h"
 
 #include "aes.h"
+
+uint8_t x_mult(uint8_t byte) { return (byte & 0x80) ? ((byte << 1) ^ 0x1b) : (byte << 1); }
+
+uint8_t g_mult(uint8_t a, uint8_t b) {
+  uint8_t result = 0;
+  uint8_t tmp = b;
+  for (int i = 0; i < 8; i++) {
+    if (a & 0x01) {
+      result ^= tmp;
+    }
+    a >>= 1;
+    tmp = x_mult(tmp);
+  }
+  return result;
+}
+
+void mix_columns_mult(uint8_t* word, uint8_t* result) {
+  result[0] = g_mult(0x02, word[0]) ^ g_mult(0x03, word[1]) ^ word[2] ^ word[3];
+  result[1] = word[0] ^ g_mult(0x02, word[1]) ^ g_mult(0x03, word[2]) ^ word[3];
+  result[2] = word[0] ^ word[1] ^ g_mult(0x02, word[2]) ^ g_mult(0x03, word[3]);
+  result[3] = g_mult(0x03, word[0]) ^ word[1] ^ word[2] ^ g_mult(0x02, word[3]);
+}
--- gmath.h
+++ gmath.h
@@ -1,7 +1,37 @@
+/**
+ * @file gmath.h
+ * @brief Rijndael's finite field arithmetic
+ * @authors Paul Bureau, Victoria Martini, Quentin Bonnel
+ **/
+
 #ifndef AES_H
 #define AES_H
 
 #include <stdint.h>
 #include <stdlib.h>
+
+/**
+ * @brief Multiplication by x in the finite field
+ * @details Take a byte and multiply it by x in the Rindjael's finite field
+ * @param byte the byte to multiply which represent a number in the finite field GF(2^8)
+ **/
+uint8_t x_mult(uint8_t byte);
+
+/**
+ * @brief Multiplication in the finite field
+ * @details Take two bytes and multiply them in the Rindjael's finite field
+ * @param a the first byte to multiply which represent a number in the finite field GF(2^8)
+ * @param b the second byte to multiply which represent a number in the finite field GF(2^8)
+ **/
+uint8_t g_mult(uint8_t a, uint8_t b);
+
+/**
+ * @brief MixColumns operation
+ * @details Multiply a column of the state by a fixed matrix
+ * @param word a column of the state
+ * @param result the result of the operation
+ * @pre @p word and @p result must be a valid pointer toward an array with a size of 4
+ **/
+void mix_columns_mult(uint8_t* word, uint8_t* result);
 
 #endif  // AES_H
