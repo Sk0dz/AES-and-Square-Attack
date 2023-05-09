21974998684786b994aa5165cc97b348e11bc0fb
Paul Bureau <paul.bureau@etu.u-bordeaux.fr>
2023-05-09 02:11:16 +0200

    C'est bon ça marche

--- attack.c
+++ attack.c
@@ -18,12 +18,12 @@ void reduced_encryption(uint8_t* state, uint8_t* round_key) {
     add_round_key(4, state, round_key);
 }
 
-uint8_t** delta_set_gen() {
+uint8_t** delta_set_gen(uint8_t same_byte) {
     uint8_t** delta_set = malloc(256 * sizeof(uint8_t*));
     for (int i = 0; i < 256; i++) {
         delta_set[i] = malloc(16 * sizeof(uint8_t));
         for (int j = 0; j < 16; j++) {
-        delta_set[i][j] = 0x00;
+        delta_set[i][j] = same_byte;
         }
     }
     for (int i = 0; i < 256; i++) {
@@ -32,63 +32,34 @@ uint8_t** delta_set_gen() {
     return delta_set;
 }
 
-uint8_t* delta_byte_extract(uint8_t** delta_set, int byte) {
-    uint8_t* delta_byte = malloc(256 * sizeof(uint8_t));
-    for (int i = 0; i < 256; i++) {
-        delta_byte[i] = delta_set[i][byte];
-    }
-    return delta_byte;
-}
 
-void inverse_sub_bytes_delta(uint8_t* delta_byte){
-    for (int i = 0; i < 256; i++) {
-        delta_byte[i] = inverse_s_box[delta_byte[i]];
+uint8_t* key_unexpand(uint8_t* guess_key){
+    uint8_t* key_storage = malloc(80 * sizeof(uint8_t));
+    for (int i=64; i<80; i++){
+        key_storage[i] = guess_key[i-64];
     }
-}
-
-uint8_t sum_of_bytes(uint8_t* delta_byte){
-    uint8_t sum = 0x00;
-    for (int i = 0; i < 256; i++) {
-        sum = sum ^ delta_byte[i];
-    }
-    return sum;
-}
-
-uint8_t key_guess(uint8_t** delta_set, int byte){
-    for (int i = 0; i < 256; i++) {
-        uint8_t key_guess = i;
-        uint8_t* delta_byte = delta_byte_extract(delta_set, byte);
-        for (int j = 0; j < 256; j++) {
-            delta_byte[j] = delta_byte[j] ^ key_guess;
+    for (int i=3; i>=0; i--){
+        for (int j=3; j>0; j--){
+            for (int k=0; k<4; k++){
+                key_storage[16*i + 4*j + k] = key_storage[16*(i+1) + 4*(j-1) + k] ^ key_storage[16*(i+1) + 4*j + k];
+            }
         }
-        inverse_sub_bytes_delta(delta_byte);
-        uint8_t sum = sum_of_bytes(delta_byte);
-        if (sum == 0x00){
-            free(delta_byte);
-            return key_guess;
+        uint8_t word[4];
+        for (int k=0; k<4; k++){
+            word[k] = key_storage[16*i + 12 + k];
         }
-        free(delta_byte);
-    }
-}
-
-
-void key_unexpand(uint8_t* key_guess) {
-  for (int i = 3; i >= 0; i--) {
-    int tmp = 0;
-    int row, column = 0;
-
-    for (row = 3; row >= 0; row--) {
-        for (column = 3; column >= 1; column--) {
-        key_guess[(4 * row) + column] ^= key_guess[(4 * row) + column - 1];
+        rot_word(word);
+        sub_word(word);
+        key_storage[16*i] = word[0] ^ key_storage[16*(i+1)] ^ Rcon[i];
+        for (int k=1; k<4; k++){
+            key_storage[16*i + k] = word[k] ^ key_storage[16*(i+1) + k];
         }
     }
+    uint8_t* key = malloc(16 * sizeof(uint8_t));
+    for (int i=0; i<16; i++){
+        key[i] = key_storage[i];
+    }
+    free(key_storage);
+    return key;
 
-    tmp = key_guess[3];
-    key_guess[0] = s_box[key_guess[7]] ^ Rcon[i] ^ key_guess[0];
-    key_guess[4] = s_box[key_guess[11]] ^ key_guess[4];
-    key_guess[8] = s_box[key_guess[15]] ^ key_guess[8];
-    key_guess[12] = s_box[tmp] ^ key_guess[12];
-        
-  }
-  
 }
--- main_attack.c
+++ main_attack.c
@@ -10,30 +10,44 @@
 
 
 int main(int argc, char** argv[]){
-    uint8_t key[] = {0xd0, 0xc9, 0xe1, 0xb6, 0x14, 0xee, 0x3f, 0x63, 0xf9, 0x25, 0x0c, 0x0c, 0xa8, 0x89, 0xc8, 0xa6};
+    uint8_t key[] = {0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7, 0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c};
     uint8_t expanded_key[176];
     key_expansion(key, expanded_key);
-    for(int i=64; i<81; i++){
+    for(int i=64; i<80; i++){
         printf("%02x ", expanded_key[i]);
     }
 
-    uint8_t** delta_set = delta_set_gen();
+    uint8_t** delta_set = delta_set_gen(0x00);
+    uint8_t** delta_set_2 = delta_set_gen(0x23);
+
     for(int i = 0; i<256; i++){
         reduced_encryption(delta_set[i], expanded_key);
+        reduced_encryption(delta_set_2[i], expanded_key);
     }
-
     uint8_t guess_key[16];
     for(int i=0; i<16; i++){
-        guess_key[i] = key_guess(delta_set, i);
+        for(int k=0; k<256; k++){
+            uint8_t s1 = 0;
+            uint8_t s2 = 0;
+
+            for(int j=0; j<256; j++){
+                s1 = inverse_s_box[delta_set[j][i] ^ k] ^ s1;
+                s2 = inverse_s_box[delta_set_2[j][i] ^ k] ^ s2;
+            }
+            if(s1 == 0 && s2 == 0){
+                guess_key[i] = k;
+            }
+        }
     }
+
     printf("Key guess: ");
     for(int i=0; i<16; i++){
         printf("%02x ", guess_key[i]);
     }
-    key_unexpand(guess_key);
+    uint8_t* key_find = key_unexpand(guess_key);
     printf("\nKey guess after rewind: ");
     for(int i=0; i<16; i++){
-        printf("%02x ", guess_key[i]);
+        printf("%02x ", key_find[i]);
     }
     return EXIT_SUCCESS;
 }
